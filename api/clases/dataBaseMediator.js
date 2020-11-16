641d297ed8f05078d31b8e8bc1c2fa9cb249a999
marcobaldi97 <60588169+marcobaldi97@users.noreply.github.com>
2020-11-16 15:17:14 -0300

    Update dataBaseMediator.js
    trying to do another ways.

--- api/clases/dataBaseMediator.js
+++ api/clases/dataBaseMediator.js
@@ -3,7 +3,8 @@ const  {Pool, Client} = require('pg');
 
 class dataBaseMediator{ 
     constructor (){
-        let actualResponse;
+        let lastSelectDBResponse;
+        let lastInsertDBResponse;
         const dbUser = 'postgres';
         const dbPassword = 'basedMaster97';
         const dbIp = 'localhost';
@@ -24,24 +25,77 @@ class dataBaseMediator{
 
     async executeSelectConsult(select_consult){
         try{
-            const functionResponse = await this.pool.query(select_consult);
-            this.actualResponse = functionResponse.rows;
+            const queryResponse = await this.pool.query(select_consult);
+            this.lastSelectDBResponse = queryResponse.rows;
         }catch(error){
             console.log(error);
         }   
     }
 
-    getActualResponse(){
-        return this.actualResponse;
+    executeSelectConsultVersion2(select_consult){
+        this.client
+            .query('SELECT NOW() as now')
+            .then(res => {
+                console.log(res.rows);
+                return res.rows;
+            })
+            .catch(e => console.error(e.stack)) 
+            .finally();
+    }
+
+    async executeInsertConsult(insert_consult){
+        try{
+            const queryResponse = await this.pool.query(insert_consult);
+            this.lastInsertDBResponse = queryResponse;
+        }catch(error){
+            console.log(error);
+        }   
+    }
+
+    getLastSelectDBResponse(){
+        return this.lastSelectDBResponse;
     }    
+
+    getLastInsertDBResponse(){
+        return this.lastInsertDBResponse;
+    }
 }//final clase
+
+/*
+let a = new dataBaseMediator();
+let b = [];
+b = a.executeSelectConsultVersion2;
+for (i = 0; i < b.length; i++) {
+    console.log('El id es: '+b[i].publication_id+' El contenido es:'+b[i].publication_content);
+}
+console.log('Wtf?');
+*/
+
 
 /*
 try{ 
     (async () => {
         let a = new dataBaseMediator();
+        let b = [];
+        b = await a.executeSelectConsult('SELECT * FROM publications');
+        console.log('↓↓↓↓↓↓ This is your response ↓↓↓↓↓↓');
+        var i;
+        for (i = 0; i < b.length; i++) {
+            console.log('El id es: '+b[i].publication_id+' El contenido es:'+b[i].publication_content);
+        }
+        console.log('Wtf?');
+    })()   
+}catch(error){
+    console.error(error);
+}
+*/
+
+/* Attempt #1
+try{ 
+    (async () => {
+        let a = new dataBaseMediator();
         await a.executeSelectConsult('SELECT * FROM publications');
-        let b = a.getActualResponse();
+        let b = a.getLastSelectDBResponse();
         console.log('↓↓↓↓↓↓ This is your response ↓↓↓↓↓↓');
         var i;
         for (i = 0; i < b.length; i++) {
@@ -52,4 +106,3 @@ try{
     console.error(error);
 }
 */
-
